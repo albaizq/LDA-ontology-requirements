9d4a94356ea6e5a6b1e3ba1e2c3cc796fa2dfdb9
albaizq <albaizq13@gmail.com>
2021-06-18 18:52:43 +0200

    add association between reqs and terms

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import modules.topicExtraction
 
 # The goal of these functions is to obtain the topics related to a set of ontology requirements.
 if __name__ == '__main__':
-    df = pd.read_csv(r"C:\Users\UX360CAK\Documents\OEG\RequirementAnalysis\dataset\requirements.csv",
+    df = pd.read_csv(r"\dataset\requirements.csv",
                      sep=r'\s*;\s*', header=0, encoding='utf-8', engine='python')
     reqs = modules.dataDescription.analyse_data(df)
     modules.topicExtraction.get_topics(reqs)
--- modules/topicExtraction.py
+++ modules/topicExtraction.py
@@ -6,6 +6,10 @@ from sklearn.decomposition import LatentDirichletAllocation
 # package to clean text
 import re
 
+stopwords = nltk.corpus.stopwords.words('english')
+word_rooter = nltk.stem.snowball.PorterStemmer(ignore_stopwords=False).stem
+punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'
+
 
 # topics
 def display_topics(model, feature_names, no_top_words):
@@ -18,11 +22,30 @@ def display_topics(model, feature_names, no_top_words):
     return pd.DataFrame(topic_dict)
 
 
+# terms association
+def obtain_terms(req):
+    req = req.lower()
+    req = re.sub('[' + punctuation + ']+', ' ', req)
+    req = re.sub(r'\s+', ' ', req)
+    req = re.sub(r'([0-9]+)', '', req)
+    stopwords.append("yes")
+
+    req_token_list = [word for word in req.split(' ')
+                      if word not in stopwords]
+
+    req_token_list_unique = pd.unique(req_token_list).tolist()
+    tags = nltk.pos_tag(req_token_list_unique)
+    nouns = []
+
+    for elem in tags:
+        if elem[1] == 'NN' and elem[0] != '':
+            nouns.append(elem[0].capitalize())
+    final = ', '.join(nouns)
+    return final
+
+
 # cleaning
 def clean_req(req):
-    stopwords = nltk.corpus.stopwords.words('english')
-    word_rooter = nltk.stem.snowball.PorterStemmer(ignore_stopwords=False).stem
-    punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@'
     req = req.lower()
     req = re.sub('[' + punctuation + ']+', ' ', req)
     req = re.sub(r'\s+', ' ', req)
@@ -51,3 +74,11 @@ def get_topics(df):
     print("3) Extracted topics with LDA")
     pd.set_option('display.max_columns', None)
     print(display_topics(model, tf_feature_names, no_top_words))
+    print("4) Terms identification")
+    new_column = df.Requirement.apply(obtain_terms)
+    df.insert(loc=3,
+              column='Terms',
+              value=new_column)
+    print(df)
+    file_name = r"\dataset\ReqsWithTerms.csv"
+    df.to_csv(file_name, encoding='utf-8', sep=';', index=False)
